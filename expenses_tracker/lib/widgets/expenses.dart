3d1a7808654ee59f20f1d7e0032828bd39b46cee
Himanshhu55 <himanshukushwah286@gmail.com>
2023-09-11 14:21:01 +0530

    some changes

--- expenses_tracker/lib/widgets/expenses.dart
+++ expenses_tracker/lib/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:expenses_tracker/models/expense.dart';
+import 'package:expenses_tracker/widgets/chart/chart.dart';
 import 'package:expenses_tracker/widgets/newExpenses.dart';
 import 'package:expenses_tracker/widgets/expenses_list.dart';
 import 'package:flutter/material.dart';
@@ -12,48 +13,83 @@ class Expenses extends StatefulWidget {
 
 class _ExpensesState extends State<Expenses> {
   final List<Expense> _registeredExpenses = [
-      Expense(
-          title: "Flutter course",
-          amount: 20.00,
-          date: DateTime.now(),
-          category: Category.food),
-      Expense(
-          title: "Cinema",
-          amount: 55.00,
-          date: DateTime.now(),
-          category: Category.leisure),
-    ];
+    Expense(
+        title: "Flutter course",
+        amount: 20.00,
+        date: DateTime.now(),
+        category: Category.food),
+    Expense(
+        title: "Cinema",
+        amount: 55.00,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
 
-    void _addExpense(Expense expense) {
-      setState(() {
-        _registeredExpenses.add(expense);
-      });
-    }
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
+  }
 
-    void _openAddExpensesOverlay() {
-      showModalBottomSheet(
-        context: context,
-        builder: (ctx) => NewExpenses(onAddExpense: _addExpense),
-      );
-    }
+  void _openAddExpensesOverlay() {
+    showModalBottomSheet(
+      useSafeArea: true,
+      isScrollControlled: true,
+      context: context,
+      builder: (ctx) => NewExpenses(onAddExpense: _addExpense),
+    );
+  }
+
+  void _removeExpenses(Expense expense) {
+    final ExpensesIndex = _registeredExpenses.indexOf(expense);
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: const Text("Expense deleted"),
+      duration: const Duration(seconds: 3),
+      action: SnackBarAction(
+        label: "Undo",
+        onPressed: () {
+          setState(() {
+            _registeredExpenses.insert(ExpensesIndex, expense);
+          });
+        },
+      ),
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    Widget mainContent =
+        const Center(child: Text("No expenses found, Start adding some"));
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpenses,
+      );
+    }
     return Scaffold(
-      appBar: AppBar(
-          actions: [
-            IconButton(
-                onPressed: _openAddExpensesOverlay, icon: const Icon(Icons.add))
-          ],
-          backgroundColor: const Color.fromARGB(255, 97, 55, 168),
-          title: const Text("Expense Tracker")),
-      body: Column(
-        children: [
-          const Text("The chart"),
-          Expanded(
-            child: ExpensesList(expenses: _registeredExpenses),
-          ),
-        ],
-      ),
-    );
+        appBar: AppBar(
+            actions: [
+              IconButton(
+                  onPressed: _openAddExpensesOverlay,
+                  icon: const Icon(Icons.add))
+            ],
+            // backgroundColor: const Color.fromARGB(255, 97, 55, 168),
+            title: const Text("Expense Tracker")),
+        body: width < 600
+            ? Column(
+                children: [
+                  Chart(expenses: _registeredExpenses),
+                  Expanded(child: mainContent)
+                ],
+              )
+            : Row(children: [
+                Expanded(child: Chart(expenses: _registeredExpenses)),
+                Expanded(child: mainContent),
+              ]));
   }
 }
