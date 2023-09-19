b21d600a2103f246eb7477d6b1bd60a7d251897a
Himanshhu55 <himanshukushwah286@gmail.com>
2023-09-19 22:03:27 +0530

    New ShoppingList app

--- shopping_list/lib/models/category.dart
+++ shopping_list/lib/models/category.dart
@@ -14,7 +14,7 @@ enum Categories {
 }
 
 class Category {
-  const Category(this.id, this.color);
-  final String id;
+  const Category(this.title, this.color);
+  final String title;
   final Color color;
 }
