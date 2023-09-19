76fc96a6a45f56322a7430e5595b168f48312720
Himanshhu55 <himanshukushwah286@gmail.com>
2023-09-19 22:02:01 +0530

    New shoppingList App

---
+++ shopping_list/lib/models/category.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+enum Categories {
+  vegetables,
+  fruit,
+  meat,
+  dairy,
+  carbs,
+  sweets,
+  spices,
+  convenience,
+  hygiene,
+  other
+}
+
+class Category {
+  const Category(this.id, this.color);
+  final String id;
+  final Color color;
+}
